5871476d7284b7d059afb2921eae732bd9ddb860
Shayne Meyer <shayne.meyer@gmail.com>
2016-11-16 15:55:58 -0800

    updated build list to have more than one item

--- src/components/BuildList.js
+++ src/components/BuildList.js
@@ -3,22 +3,71 @@ import BuildItem from './BuildItem';
 
 class BuildList extends React.Component {
   render() {
-    let buildMock = {
-      webUrl: "https://boo.com",
-      name: "Build",
-      finishTime: "11/2/16 9:48 AM",
-      startTime: "11/2/16 9:40 AM",
-      changeName: "NOTICKET Version bump 1.14.51",
-      isDisabled: false,
-      isRunning: false,
-      statusText: 'Testing the status text'
+    let buildMock = [
+      {
+        id: 1,
+        webUrl: "https://boo.com",
+        name: "Build",
+        finishTime: "11/2/16 9:48 AM",
+        startTime: "11/2/16 9:40 AM",
+        changeName: "NOTICKET Version bump 1.14.51",
+        isDisabled: false,
+        isRunning: false,
+        statusText: 'Testing the status text'
+      },
+      {
+        id: 2,
+        webUrl: "https://boo.com",
+        name: "Build",
+        finishTime: "11/2/16 9:48 AM",
+        startTime: "11/2/16 9:40 AM",
+        changeName: "NOTICKET Version bump 1.14.51",
+        isDisabled: false,
+        isRunning: false,
+        statusText: 'Testing the status text'
+      },
+      {
+        id: 3,
+        webUrl: "https://boo.com",
+        name: "Build",
+        finishTime: "11/2/16 9:48 AM",
+        startTime: "11/2/16 9:40 AM",
+        changeName: "NOTICKET Version bump 1.14.51",
+        isDisabled: false,
+        isRunning: false,
+        statusText: 'Testing the status text'
+      },
+      {
+        id: 4,
+        webUrl: "https://boo.com",
+        name: "Build",
+        finishTime: "11/2/16 9:48 AM",
+        startTime: "11/2/16 9:40 AM",
+        changeName: "NOTICKET Version bump 1.14.51",
+        isDisabled: false,
+        isRunning: false,
+        statusText: 'Testing the status text'
+      },
+      {
+        id: 5,
+        webUrl: "https://boo.com",
+        name: "Build",
+        finishTime: "11/2/16 9:48 AM",
+        startTime: "11/2/16 9:40 AM",
+        changeName: "NOTICKET Version bump 1.14.51",
+        isDisabled: false,
+        isRunning: true,
+        statusText: 'Testing the status text'
+      },
+    ]
 
-    }
-
+    let mocks = buildMock.map((item) => {
+      return <BuildItem key={item.id} build={item} />
+    })
     return (
       <div>
         <h1>BuildItems:</h1>
-        <BuildItem build={buildMock} />
+        {mocks}
       </div>
     );
   }
